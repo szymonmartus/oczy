d15bfb1612239bc35932334f0d6d35439818ec8e
szymonmartus <szymon.martus93@gmail.com>
2021-05-14 19:14:08 +0200

    unfollow

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const showImg = (attr) => {
   main.innerHTML = `<img class='big' src=${attr}></img>`;
   btnz.style.display = "flex";
   currentImg = attr;
+  console.log(attr);
 };
 
 const unfriend = () => {
@@ -81,76 +82,6 @@ const mute = () => {
   setTimeout(muteConversations, 1000);
 };
 
-const unfollow = () => {
-  main.innerHTML = `<div id="unfollow"></div>`;
-  var trailimage = [currentImg, 100, 99]; //image path, plus width and height
-  var offsetfrommouse = [10, -20]; //image x,y offsets from cursor position in pixels. Enter 0,0 for no offset
-  var displayduration = 10; //duration in seconds image should remain visible. 0 for always.
-
-  if (document.getElementById || document.all)
-    newDiv = document.createElement("div");
-  newDiv.innerHTML =
-    '<div id="trailimageid" style="position:absolute;visibility:visible;left:0px;top:0px;width:1px;height:1px"><img src="' +
-    trailimage[0] +
-    '" border="0" width="' +
-    trailimage[1] +
-    'px" height="' +
-    trailimage[2] +
-    'px"></div>';
-
-  mainDiv = document.getElementById("main");
-  document.body.insertBefore(newDiv, mainDiv);
-
-  function gettrailobj() {
-    if (document.getElementById)
-      return document.getElementById("trailimageid").style;
-    else if (document.all) return document.all.trailimagid.style;
-  }
-
-  function truebody() {
-    return !window.opera &&
-      document.compatMode &&
-      document.compatMode != "BackCompat"
-      ? document.documentElement
-      : document.body;
-  }
-
-  function hidetrail() {
-    gettrailobj().visibility = "hidden";
-    document.onmousemove = "";
-  }
-
-  function followmouse(e) {
-    var xcoord = offsetfrommouse[0];
-    var ycoord = offsetfrommouse[1];
-    if (typeof e != "undefined") {
-      xcoord += e.pageX;
-      ycoord += e.pageY;
-    } else if (typeof window.event != "undefined") {
-      xcoord += truebody().scrollLeft + event.clientX;
-      ycoord += truebody().scrollTop + event.clientY;
-    }
-    var docwidth = document.all
-      ? truebody().scrollLeft + truebody().clientWidth
-      : pageXOffset + window.innerWidth - 15;
-    var docheight = document.all
-      ? Math.max(truebody().scrollHeight, truebody().clientHeight)
-      : Math.max(document.body.offsetHeight, window.innerHeight);
-    if (
-      xcoord + trailimage[1] + 3 > docwidth ||
-      ycoord + trailimage[2] > docheight
-    )
-      gettrailobj().display = "none";
-    else gettrailobj().display = "";
-    gettrailobj().left = xcoord + "px";
-    gettrailobj().top = ycoord + "px";
-  }
-
-  document.onmousemove = followmouse;
-
-  if (displayduration > 0) setTimeout(hidetrail, displayduration * 1000);
-};
-
 const block = () => {
   let count = 0;
   main.innerHTML = `<div id="main"></div>`;
@@ -168,10 +99,113 @@ const block = () => {
 
     main.appendChild(img);
     count += 1;
-    if (count == 300) {
+    if (count == 250) {
       clearInterval(runBlock);
       main.innerHTML = `<div id="main"></div>`;
     }
   }
   const runBlock = setInterval(show_image, 50);
 };
+
+var followCursor = (function () {
+  s = document.createElement("img");
+  s.style.position = "absolute";
+  s.style.margin = "0";
+  s.style.padding = "5px";
+  s.src = currentImg;
+
+  return {
+    init: function () {
+      s.src = currentImg;
+      document.body.appendChild(s);
+    },
+
+    run: function (e) {
+      var e = e || window.event;
+      s.style.left = e.clientX + 15 + "px";
+      s.style.top = e.clientY + 15 + "px";
+    },
+  };
+})();
+const clearFollow = () => {
+  s.remove();
+};
+
+const unfollow = () => {
+  main.innerHTML = "<div></div>";
+  followCursor.init();
+  document.body.onmousemove = followCursor.run;
+  setTimeout(clearFollow, 5000);
+};
+
+// const unfollow = () => {
+//   main.innerHTML = `<div id="unfollow"></div>`;
+//   var trailimage = [currentImg, 100, 99]; //image path, plus width and height
+//   var offsetfrommouse = [10, -20]; //image x,y offsets from cursor position in pixels. Enter 0,0 for no offset
+//   var displayduration = 2; //duration in seconds image should remain visible. 0 for always.
+
+//   if (document.getElementById || document.all)
+//     newDiv = document.createElement("div");
+//   newDiv.innerHTML =
+//     '<div id="trailimageid" style="position:absolute;visibility:visible;left:0px;top:0px;width:1px;height:1px"><img src="' +
+//     trailimage[0] +
+//     '" border="0" width="' +
+//     trailimage[1] +
+//     'px" height="' +
+//     trailimage[2] +
+//     'px"></div>';
+
+//   mainDiv = document.getElementById("main");
+//   document.body.insertBefore(newDiv, mainDiv);
+
+//   function gettrailobj() {
+//     if (document.getElementById)
+//       return document.getElementById("trailimageid").style;
+//     else if (document.all) return document.all.trailimagid.style;
+//   }
+
+//   function truebody() {
+//     return !window.opera &&
+//       document.compatMode &&
+//       document.compatMode != "BackCompat"
+//       ? document.documentElement
+//       : document.body;
+//   }
+
+//   function hidetrail() {
+//     document.getElementById("trailimageid").innerHTML = "<div></div>";
+
+//     // gettrailobj().visibility = "hidden";
+//     // document.onmousemove = "";
+//   }
+
+//   function followmouse(e) {
+//     var xcoord = offsetfrommouse[0];
+//     var ycoord = offsetfrommouse[1];
+//     if (typeof e != "undefined") {
+//       xcoord += e.pageX;
+//       ycoord += e.pageY;
+//     } else if (typeof window.event != "undefined") {
+//       xcoord += truebody().scrollLeft + event.clientX;
+//       ycoord += truebody().scrollTop + event.clientY;
+//     }
+//     var docwidth = document.all
+//       ? truebody().scrollLeft + truebody().clientWidth
+//       : pageXOffset + window.innerWidth - 15;
+//     var docheight = document.all
+//       ? Math.max(truebody().scrollHeight, truebody().clientHeight)
+//       : Math.max(document.body.offsetHeight, window.innerHeight);
+//     if (
+//       xcoord + trailimage[1] + 3 > docwidth ||
+//       ycoord + trailimage[2] > docheight
+//     )
+//       gettrailobj().display = "none";
+//     else gettrailobj().display = "";
+//     gettrailobj().left = xcoord + "px";
+//     gettrailobj().top = ycoord + "px";
+//   }
+
+//   document.onmousemove = followmouse;
+
+//   if (displayduration > 0) setTimeout(hidetrail, displayduration * 1000);
+// };
